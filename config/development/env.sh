19722a23588c0ec22b2cf9a87368a41b21bbe612
Jeff <jeff10278@me.com>
2015-11-18 20:42:45 -0600

    added kadria

--- config/development/env.sh
+++ config/development/env.sh
@@ -7,3 +7,5 @@
 # export ROOT_URL="http://localhost:3000"
 
 export MAIL_URL="smtp://postmaster@sandboxa8757c7aa6204f9980765403c5f9cf91.mailgun.org:secret102@smtp.mailgun.org:587"
+export KADIRA_APP_ID=ZpYeiP2WCimRLGwSQ
+export KADIRA_APP_SECRET=81624e7b-e0ee-460f-9545-aff0177692bb
